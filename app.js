7ca66875bcb2b8f6d8454a6953dc03218af4ddf3
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-02-03 08:43:46 -0500

    Changed some silly mistakes

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
-
-var MY_KEY = config.MY_KEY;
+var my_key = config.MY_KEY;
 
 $(document).ready(function() {
-    $("#submit").click(function() {
-      var city = $("#city").val();
-      var apiKey = "MY_KEY";
-      var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-  
-      $.ajax({
-        url: apiUrl,
-        success: function(response) {
-          var temperature = response.main.temp;
-          var conditions = response.weather[0].description;
-          $("#weather-info").html("Temperature: " + temperature + "°C<br>" + "Conditions: " + conditions);
-        }
-      });
+  $("#getWeather").click(function() {
+    var city = $("#city").val();
+    var apiKey = "my_key";
+    var apiUrl =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&appid=" +
+      apiKey;
+
+    $.getJSON(apiUrl, function(data) {
+      var weather = data.weather[0].description;
+      var temp = data.main.temp - 273.15;
+      var humidity = data.main.humidity;
+      var wind = data.wind.speed;
+
+      $("#output").html(
+        "Weather: " +
+          weather +
+          "<br>Temperature: " +
+          temp.toFixed(2) +
+          "°C" +
+          "<br>Humidity: " +
+          humidity +
+          "%" +
+          "<br>Wind Speed: " +
+          wind +
+          "m/s"
+      );
     });
   });
-  
+});
