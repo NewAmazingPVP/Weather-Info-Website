7b6f2b90b9edbb0d445e2deb824f0bb9ecbc21b0
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-02-03 09:09:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-
+var key = config.MY_KEY
 
 $(document).ready(function() {
   $("#getWeather").click(function() {
     var city = $("#city").val();
-    var apiKey = "af527d7292e58a1b188954167ce7b7d2";
+    var apiKey = "MY_KEY";
     var apiUrl =
       "https://api.openweathermap.org/data/2.5/weather?q=" +
       city +
