936ccbe271287ea7665d28dda4fc5728343d6422
NewAmazingPVP <gamingofpat@gmail.com>
2023-03-25 12:43:47 -0400

    Added comments and more output's such as sunrise hour in EST
    Also added "Enter the City" message

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
+// Wait for the document to be fully loaded before running the script
 $(document).ready(function() {
+
+  // Listen for a click on the "Get Weather" button
   $("#getWeather").click(function() {
+
+    // Get the value entered in the input field with ID "city"
     var city = $("#city").val();
-    //please dont steal the api key this project was purely made to help myself learn
+
+    // Set the API key to be used to retrieve weather data
+    // NOTE: Do not copy this key, it is for demonstration purposes only
     var apiKey = "af527d7292e58a1b188954167ce7b7d2";
+
+    // Construct the URL for the API request using the city and API key
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
+    // Send a GET request to the API and execute a callback function when the response is received
     $.getJSON(apiUrl, function(data) {
+
+      // Extract the weather description from the API response
       var weather = data.weather[0].description;
+
+      // Convert the temperature from Kelvin to Celsius and round to 2 decimal places
       var temp = data.main.temp - 273.15;
+
+      // Extract the humidity value from the API response
       var humidity = data.main.humidity;
+
+      // Extract the wind speed value from the API response
       var wind = data.wind.speed;
 
+      var sunrise = data.sys.sunrise;
+
+      var sunriseDate = new Date(sunrise * 1000); // create new Date object from Unix timestamp
+      var sunriseHour = sunriseDate.getHours(); // get hour value from Date object
+      var sunriseMinute = sunriseDate.getMinutes(); // get minute value from
+      if (sunriseMinute < 10)
+      {
+        sunriseMinute = "0" + sunriseMinute;
+      }
+
+
+      // Display the weather data in the HTML element with ID "output"
       $("#output").html(
         "Weather: " +
           weather +
@@ -22,7 +52,11 @@ $(document).ready(function() {
           "%" +
           "<br>Wind Speed: " +
           wind +
-          "m/s"
+          "m/s" +
+          "<br> Sunrise: " +
+          sunriseHour +
+          ":" +
+          sunriseMinute + " est"
       );
     });
   });
