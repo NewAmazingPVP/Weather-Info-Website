19936ec3e301b2f1e68744a47b55556a225f9b0c
Parth <87737759+NewAmazingPVP@users.noreply.github.com>
2023-03-31 11:29:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     var city = $("#city").val();
 
     // Set the API key to be used to retrieve weather data
-    var apiKey = ${{ secrets.WEATHER_API_KEY }};
+    var apiKey = process.env.WEATHER_API_KEY;
 
     // Construct the URL for the API request using the city and API key
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
